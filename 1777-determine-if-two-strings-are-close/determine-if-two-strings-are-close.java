7f5f09e5bee5d71c3378d1b29144f79db4408a86
smkrishna27 <smkrishna27@gmail.com>
2026-07-05 23:03:55 +0530

    Time: 11 ms (85.20%) | Memory: 47.8 MB (84.48%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean closeStrings(String word1, String word2) {
          if (word1.length() != word2.length()) return false;
 
-        Map<Character, Integer> freq1 = new HashMap<>();
+       /*Map<Character, Integer> freq1 = new HashMap<>();
         Map<Character, Integer> freq2 = new HashMap<>();
 
         for (char c : word1.toCharArray()) {
@@ -20,7 +20,24 @@ class Solution {
         Collections.sort(list1);
         Collections.sort(list2);
 
-        return list1.equals(list2);
+        return list1.equals(list2);*/
+         int[] freq1 = new int[26];
+    int[] freq2 = new int[26];
+
+    for (char c : word1.toCharArray()) freq1[c - 'a']++;
+    for (char c : word2.toCharArray()) freq2[c - 'a']++;
+
+    // Check character sets
+    for (int i = 0; i < 26; i++) {
+        if ((freq1[i] == 0 && freq2[i] != 0) || (freq1[i] != 0 && freq2[i] == 0)) {
+            return false;
+        }
+    }
+
+    Arrays.sort(freq1);
+    Arrays.sort(freq2);
+
+    return Arrays.equals(freq1, freq2);
         
     }
 }

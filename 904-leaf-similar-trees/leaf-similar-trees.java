f64391ef0f863ecae8901ec288f94acd3d8f0888
smkrishna27 <smkrishna27@gmail.com>
2026-07-07 19:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (96.07%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+     if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+    
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        dfsSearch(root1, list1);
+        dfsSearch(root2, list2);
+        return list1.equals(list2);
+
+    }
+
+    private  void dfsSearch(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return;
+        }
+        dfsSearch(root.left, list);
+        dfsSearch(root.right, list);
+
+    }
+}

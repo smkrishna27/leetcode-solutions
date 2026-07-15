8ba6d9d0da8c2f60edd5cdc5f2445af60b7f1703
smkrishna27 <smkrishna27@gmail.com>
2026-07-15 15:08:52 +0530

    Time: 11 ms (51.73%) | Memory: 46 MB (79.05%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+         if (s == null || s.isEmpty()  || k > s.length()) {
+            return 0;
+        }
+        int[] freq = new int[26];
+        int left = 0;
+        int maxFreq = 0;
+        int maxWindow = 0;
+        for (int right = 0; right < s.length(); right++) {
+            freq[s.charAt(right) - 'A']++;
+            maxFreq = Math.max(maxFreq, freq[s.charAt(right) - 'A']);
+            int windowLength = right - left + 1;
+
+            if (windowLength - maxFreq > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+            windowLength = right - left + 1;
+            maxWindow = Math.max(maxWindow, windowLength);
+
+        }
+        return maxWindow;
+        
+    }
+}

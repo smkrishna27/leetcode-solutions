6245694804401c3d5faff3b1e95f38ca63791176
smkrishna27 <smkrishna27@gmail.com>
2026-07-24 22:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (14.81%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,24 +15,24 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-          List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+          List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        rightView(root, res, 0);
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        return res;
 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (i == size - 1) { // last node in this level
-                    result.add(node.val);
-                }
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-            }
-        }
-        return result;
-        
     }
+
+    private static void rightView(TreeNode root, List<Integer> res, int i) {
+        if (root == null) return;
+
+        if (i == res.size() ) {
+            res.add(root.val);
+        }
+        rightView(root.right, res, i + 1);
+
+        rightView(root.left, res, i + 1);
+    }
+        
+    
 }
